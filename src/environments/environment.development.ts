4c5114fac2286a006c8e1b0e670473e89a0e4684
Gerardo Palacin <gerardopalacin.github@gmail.com>
2026-09-17 10:01:50 -0500

    feat(ui): update app files and add environments directory

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  newsProviderApiBaseUrl: 'https://newsapi.org/v2',
+  newsProviderNewsEndpointPath: '/top-headlines',
+  newsProviderSourcesEndpointPath: '/top-headlines/sources',
+  newsProviderApiKey: 'YOUR_DEVELOPMENT_API_KEY', // Replace with your actual development API key
+  logoProviderApiBaseUrl: 'https://img.logo.dev/',
+  logoProviderPublishableKey: 'YOUR_DEVELOPMENT_LOGO_API_KEY', // Replace with your actual development logo API key
+};
