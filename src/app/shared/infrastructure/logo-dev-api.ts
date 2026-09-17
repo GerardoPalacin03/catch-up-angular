83bd1f78cd78177aa5d6010400d9ec00d616b8b3
Gerardo Palacin <gerardopalacin.github@gmail.com>
2026-09-17 10:44:49 -0500

    feat(ui): update app configurations

---
+++ src/app/shared/infrastructure/logo-dev-api.ts
@@ -0,0 +1,14 @@
+import { Service } from '@angular/core';
+import { environment } from '../../../../environments/environment';
+
+@Service()
+export class LogoDevApi {
+  baseUrl: string = environment.logoProviderApiBaseUrl;
+  apiKey: string = environment.logoProviderPublishableKey;
+
+  constructor() {}
+
+  getUrlToLogo = (url: string): string => {
+    return `${this.baseUrl}${new URL(url).hostname}?token=${this.apiKey}`;
+  };
+}
